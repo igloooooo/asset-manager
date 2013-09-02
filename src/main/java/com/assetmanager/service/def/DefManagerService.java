39c3b2484cc9054935c28b784270c6df0fd456df
igloooooo <nichols.zu@gmail.com>
2013-09-02 19:50:31 +1000

    def manager service

--- src/main/java/com/assetmanager/service/def/DefManagerService.java
+++ src/main/java/com/assetmanager/service/def/DefManagerService.java
@@ -1,9 +1,11 @@
 package com.assetmanager.service.def;
 
+import com.assetmanager.model.asset.def.BaseAssetDef;
 import com.assetmanager.service.dao.GenericDAO;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,6 +18,18 @@ import javax.annotation.Resource;
 public class DefManagerService {
     @Resource
     private GenericDAO genericDAO;
+   public List<BaseAssetDef> getAssetDefList(){
+       return (List<BaseAssetDef>)genericDAO.findAll(BaseAssetDef.class);
+   }
 
+    public <T extends BaseAssetDef> List<T> getAssertDefList(Class<T> clazz)
+    {
+        String sql = "from " + clazz.getName();
+        return(List<T>) genericDAO.findByQuery(sql, null, clazz);
+    }
 
+    public <T extends BaseAssetDef> T createAssetDef(T def)
+    {
+        return genericDAO.save(def);
+    }
 }
